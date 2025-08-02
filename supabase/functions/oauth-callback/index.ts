20db214f25b108cffb7915512b4fbad3a41702f1
JadaeHub <jadaesamuel@googlemail.com>
2025-08-02 18:25:40 +0100

    Update index.ts

--- supabase/functions/oauth-callback/index.ts
+++ supabase/functions/oauth-callback/index.ts
@@ -1,3 +1,9 @@
+
+// v2 - forcing a redeploy
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
+// ... the rest of the code is the same
 // supabase/functions/oauth-callback/index.ts
 
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
